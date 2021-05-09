13ecee59a66a38831124eada552dfb14f091e404
PashaPechenyi <pashapechenn@gmail.com>
2021-05-09 18:20:04 +0300

    Updates

---
+++ precache-manifest.ee0aa62a911168346dd9eefdab865664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561906479483818485f1da79223dcb43",
+    "url": "/itop1000-test/index.html"
+  },
+  {
+    "revision": "ceb0245f355dbdac383a",
+    "url": "/itop1000-test/static/css/main.ee2de43d.chunk.css"
+  },
+  {
+    "revision": "b9108444b8514c36d5af",
+    "url": "/itop1000-test/static/js/2.977d6815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/itop1000-test/static/js/2.977d6815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb0245f355dbdac383a",
+    "url": "/itop1000-test/static/js/main.bfd3feee.chunk.js"
+  },
+  {
+    "revision": "82c52c01ceb1301203aa",
+    "url": "/itop1000-test/static/js/runtime-main.a1fa9a77.js"
+  }
+]);
